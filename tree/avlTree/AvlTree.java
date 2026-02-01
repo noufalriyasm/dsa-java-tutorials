c042563978fa436dac20294780c87cc396074fbc
noufalriyasm <makkatnoufalriyas@gmail.com>
2026-02-01 22:50:26 +0530

    avl tree insert

--- tree/avlTree/AvlTree.java
+++ tree/avlTree/AvlTree.java
@@ -65,6 +65,78 @@ public class AvlTree {
         } else {
             return searchMethod(node.right, value);
         }
+    }
+
+    public int getHeight(AvlNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    public AvlNode rotateRight(AvlNode disbalancedNode) {
+        AvlNode newRoot = disbalancedNode.left;
+        disbalancedNode.left = disbalancedNode.left.right;
+        newRoot.right = disbalancedNode;
+        disbalancedNode.height = 1 + Math.max(getHeight(disbalancedNode.left), getHeight(disbalancedNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    public AvlNode rotateLeft(AvlNode disbalancedNode) {
+        AvlNode newRoot = disbalancedNode.right;
+        disbalancedNode.right = disbalancedNode.right.left;
+        newRoot.left = disbalancedNode;
+        disbalancedNode.height = 1 + Math.max(getHeight(disbalancedNode.left), getHeight(disbalancedNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    public int getBalance(AvlNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private AvlNode insertNode(AvlNode node, int nodeValue) {
+        if (node == null) {
+            AvlNode newNode = new AvlNode();
+            newNode.value = nodeValue;
+            newNode.height = 1;
+            return newNode;
+        } else if (nodeValue < node.value) {
+            node.left = insertNode(node.left, nodeValue);
+
+        } else {
+            node.right = insertNode(node.right, nodeValue);
+
+        }
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balance = getBalance(node);
+
+        if (balance > 1 && nodeValue < node.left.value) {
+            return rotateRight(node);
+
+        }
+        if (balance > 1 && nodeValue > node.left.value) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+        if (balance < -1 && nodeValue > node.right.value) {
+            return rotateLeft(node);
+        }
+        if (balance < -1 && nodeValue < node.right.value) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        return node;
 
     }
+
+    public void insert(int value) {
+        root = insertNode(root, value);
+    }
+
 }
